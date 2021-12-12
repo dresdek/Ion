16bf6cdbdcb08d38ba454794a59064bceb0057ae
trainb0y1 <66213737+trainb0y1@users.noreply.github.com>
2021-12-12 15:23:51 -0700

    Tab the java

--- src/main/kotlin/net/starlegacy/feature/gas/collectionfactors/HyperspaceOnlyFactor.java
+++ src/main/kotlin/net/starlegacy/feature/gas/collectionfactors/HyperspaceOnlyFactor.java
@@ -4,8 +4,8 @@ import org.bukkit.Location;
 
 public class HyperspaceOnlyFactor extends CollectionFactor {
 
-    @Override
-    public boolean factor(Location location) {
-        return location.getWorld().getName().contains("Hyperspace");
-    }
+	@Override
+	public boolean factor(Location location) {
+		return location.getWorld().getName().contains("Hyperspace");
+	}
 }
