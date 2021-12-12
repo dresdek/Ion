cb4574e9223950d47dafcc151687248d67102c96
Peter-Crawley <peterzcrawley@gmail.com>
2021-12-12 17:39:36 +0000

    Update shadow 6.0 > 7.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     java
     id("org.jetbrains.kotlin.jvm") version "1.5.0"
-    id("com.github.johnrengelman.shadow") version "6.0.0"
+    id("com.github.johnrengelman.shadow") version "7.1.0"
 }
 
 group = "net.starlegacy"
