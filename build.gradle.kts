7a576f36c7572b8ea3a7c85c226564e697fdec26
Peter-Crawley <peterzcrawley@gmail.com>
2021-12-12 16:43:23 +0000

    Reorganise dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -60,62 +60,29 @@ dependencies {
     compileOnly("com.destroystokyo.paper:paper:1.16.4-R0.1-SNAPSHOT")
 
     compileOnly("net.luckperms:api:5.0")
-
-    // https://github.com/MilkBowl/Vault
-    compileOnly("com.github.MilkBowl:VaultAPI:1.7.1")
-
-    // https://github.com/bloodmc/GriefDefender/
-    compileOnly("com.github.bloodmc:GriefDefenderAPI:master")
-
+    compileOnly("com.github.MilkBowl:VaultAPI:1.7.1") // https://github.com/MilkBowl/Vault
+    compileOnly("com.github.bloodmc:GriefDefenderAPI:master") // https://github.com/bloodmc/GriefDefender/
     compileOnly("com.discordsrv:discordsrv:1.20.0")
+    compileOnly("com.sk89q.worldedit:worldedit-bukkit:7.2.0-SNAPSHOT") // https://github.com/EngineHub/WorldEdit
+    compileOnly("net.citizensnpcs:citizens:2.0.27-SNAPSHOT") // https://github.com/CitizensDev/Citizens2/
 
     // https://github.com/webbukkit/dynmap
     compileOnly("us.dynmap:dynmap-api:3.1")
     compileOnly("us.dynmap:spigot:3.1")
 
-    // https://github.com/EngineHub/WorldEdit
-    compileOnly("com.sk89q.worldedit:worldedit-bukkit:7.2.0-SNAPSHOT")
-
-    // https://github.com/CitizensDev/Citizens2/
-    compileOnly("net.citizensnpcs:citizens:2.0.27-SNAPSHOT")
-
-    // https://kotlinlang.org/
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-
-    // https://github.com/davethomas11/MoshiPack
-    implementation("com.daveanthonythomas.moshipack:moshipack:1.0.0-beta")
-
-    // https://github.com/Litote/kmongo
-    implementation("org.litote.kmongo:kmongo:4.2.3")
-
-    // https://github.com/xetorthio/jedis
-    implementation("redis.clients:jedis:2.9.0")
-
-    // https://github.com/config4k/config4k
-    implementation("io.github.config4k:config4k:0.4.1")
-
-    // https://github.com/npgall/cqengine
-    implementation("com.googlecode.cqengine:cqengine:3.0.0")
-
-    // https://github.com/jkcclemens/khttp
-    implementation("khttp:khttp:1.0.0")
-
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8") // https://kotlinlang.org/
+    implementation("com.daveanthonythomas.moshipack:moshipack:1.0.0-beta") // https://github.com/davethomas11/MoshiPack
+    implementation("org.litote.kmongo:kmongo:4.2.3") // https://github.com/Litote/kmongo
+    implementation("redis.clients:jedis:2.9.0") // https://github.com/xetorthio/jedis
+    implementation("io.github.config4k:config4k:0.4.1") // https://github.com/config4k/config4k
+    implementation("com.googlecode.cqengine:cqengine:3.0.0") // https://github.com/npgall/cqengine
+    implementation("khttp:khttp:1.0.0") // https://github.com/jkcclemens/khttp
     implementation("net.wesjd:anvilgui:1.5.0-SNAPSHOT")
-
-    // https://github.com/stefvanschie/IF
-    implementation("com.github.stefvanschie.inventoryframework:IF:0.5.8")
-
-    // https://github.com/aikar/commands
-    implementation("co.aikar:acf-paper:0.5.0-SNAPSHOT")
-
-    // https://github.com/egg82/EventChain
-    implementation("ninja.egg82:event-chain-bukkit:1.0.7")
-
+    implementation("com.github.stefvanschie.inventoryframework:IF:0.5.8") // https://github.com/stefvanschie/IF
+    implementation("co.aikar:acf-paper:0.5.0-SNAPSHOT") // https://github.com/aikar/commands
+    implementation("ninja.egg82:event-chain-bukkit:1.0.7") // https://github.com/egg82/EventChain
     implementation("org.ejml:ejml-all:0.40")
-
-    // https://github.com/MinnDevelopment/discord-webhooks
-    implementation("club.minnced:discord-webhooks:0.5.6")
-
+    implementation("club.minnced:discord-webhooks:0.5.6") // https://github.com/MinnDevelopment/discord-webhooks
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.5.0")
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.0")
 }
