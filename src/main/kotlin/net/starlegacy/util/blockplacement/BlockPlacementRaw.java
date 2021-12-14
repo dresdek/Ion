e6630569beeaafcfe6f11747c8f27fe6d10198df
PeterCrawley1 <peterzcrawley@gmail.com>
2021-12-14 14:56:12 +0000

    Solve more NMS errors

--- src/main/kotlin/net/starlegacy/util/blockplacement/BlockPlacementRaw.java
+++ src/main/kotlin/net/starlegacy/util/blockplacement/BlockPlacementRaw.java
@@ -17,6 +17,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import net.minecraft.world.level.chunk.LevelChunk
 
 import java.util.ArrayList;
 import java.util.Map;
