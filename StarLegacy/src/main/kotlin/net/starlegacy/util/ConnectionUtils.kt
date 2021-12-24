cfa5bfc97feda3ad9f81d702dee5f6bef9ece1df
Peter-Crawley <peterzcrawley@gmail.com>
2021-12-24 19:13:07 +0000

    Try NMS move player

--- StarLegacy/src/main/kotlin/net/starlegacy/util/ConnectionUtils.kt
+++ StarLegacy/src/main/kotlin/net/starlegacy/util/ConnectionUtils.kt
@@ -1,89 +1,109 @@
 package net.starlegacy.util
 
-import net.minecraft.network.protocol.game.ClientboundPlayerPositionPacket
 import net.minecraft.network.protocol.game.ClientboundPlayerPositionPacket.RelativeArgument.X
 import net.minecraft.network.protocol.game.ClientboundPlayerPositionPacket.RelativeArgument.X_ROT
 import net.minecraft.network.protocol.game.ClientboundPlayerPositionPacket.RelativeArgument.Y
 import net.minecraft.network.protocol.game.ClientboundPlayerPositionPacket.RelativeArgument.Y_ROT
 import net.minecraft.network.protocol.game.ClientboundPlayerPositionPacket.RelativeArgument.Z
-import net.minecraft.server.network.ServerGamePacketListenerImpl
-import net.minecraft.world.phys.Vec3
 import org.bukkit.Location
 import org.bukkit.craftbukkit.v1_17_R1.entity.CraftPlayer
 import org.bukkit.entity.Player
+import org.bukkit.event.player.PlayerTeleportEvent
 import org.bukkit.util.Vector
-import java.lang.reflect.Field
 
 object ConnectionUtils {
 	private val OFFSET_DIRECTION = setOf(X_ROT, Y_ROT)
 	private val OFFSET_ALL = setOf(X_ROT, Y_ROT, X, Y, Z)
 
-	private var justTeleportedField: Field = getField("justTeleported") // I do not know the obfuscated name of this field, lets hope this just works
-	private var teleportPosField: Field = getField("y") // awaitingPositionFromClient / teleportPos
-	private var lastPosXField: Field = getField("o") // lastPosX / lastGoodX
-	private var lastPosYField: Field = getField("u") // lastPosY / lastGoodY
-	private var lastPosZField: Field = getField("q") // lastPosZ / lastGoodZ
-	private var teleportAwaitField: Field = getField("z") // awaitingTeleport / teleportAwait
-	private var AField: Field = getField("A")
-	private var eField: Field = getField("e")
+//	private var justTeleportedField: Field = getField("justTeleported") // I do not know the obfuscated name of this field, lets hope this just works
+//	private var teleportPosField: Field = getField("y") // awaitingPositionFromClient / teleportPos
+//	private var lastPosXField: Field = getField("o") // lastPosX / lastGoodX
+//	private var lastPosYField: Field = getField("u") // lastPosY / lastGoodY
+//	private var lastPosZField: Field = getField("q") // lastPosZ / lastGoodZ
+//	private var teleportAwaitField: Field = getField("z") // awaitingTeleport / teleportAwait
+//	private var AField: Field = getField("A")
+//	private var eField: Field = getField("e")
 
-	@Throws(NoSuchFieldException::class)
-	private fun getField(name: String): Field {
-		val field = ServerGamePacketListenerImpl::class.java.getDeclaredField(name)
-		field.isAccessible = true
-		return field
-	}
+//	@Throws(NoSuchFieldException::class)
+//	private fun getField(name: String): Field {
+//		val field = ServerGamePacketListenerImpl::class.java.getDeclaredField(name)
+//		field.isAccessible = true
+//		return field
+//	}
 
 	fun move(player: Player, loc: Location, theta: Float = 0.0f, offsetPos: Vector? = null) {
-		val handle = (player as CraftPlayer).handle
-		val connection = handle.connection
-		val x = loc.x
-		val y = loc.y
-		val z = loc.z
-
-		if (handle.containerMenu !== handle.inventoryMenu) {
-			handle.closeContainer()
-		}
-
-		var teleportAwait: Int
-		justTeleportedField.set(connection, true)
-		teleportPosField.set(connection, Vec3(x, y, z))
-		lastPosXField.set(connection, x)
-		lastPosYField.set(connection, y)
-		lastPosZField.set(connection, z)
-		teleportAwait = teleportAwaitField.getInt(connection).plus(1)
-
-		if (teleportAwait == 2147483647) {
-			teleportAwait = 0
-		}
-
-		teleportAwaitField.set(connection, teleportAwait)
-		AField.set(connection, eField.get(connection))
+		val connection = (player as CraftPlayer).handle.connection
 
 		val px: Double
 		val py: Double
 		val pz: Double
 
 		if (offsetPos == null) {
-			px = x
-			py = y
-			pz = z
+			px = loc.x
+			py = loc.y
+			pz = loc.z
 		} else {
 			px = offsetPos.x
 			py = offsetPos.y
 			pz = offsetPos.z
 		}
 
-		handle.setPos(x, y, z)
-		handle.setRot(handle.yRot + theta, handle.xRot)
-
-//		handle.level.updateChunkPos(handle) // Removed with 1.17.1
-
 		val flags = if (offsetPos != null) OFFSET_ALL else OFFSET_DIRECTION
-		val packet = ClientboundPlayerPositionPacket(px, py, pz, theta, 0f, flags, 0, true)
-//		old:	ClientboundPlayerPositionPacket(px, py, pz, theta, 0f, flags, teleportAwait)
-		connection.send(packet)
+
+		connection.a(px, py, pz, theta, 0f, flags, PlayerTeleportEvent.TeleportCause.PLUGIN)
+
 	}
+
+//	fun move(player: Player, loc: Location, theta: Float = 0.0f, offsetPos: Vector? = null) {
+//		val handle = (player as CraftPlayer).handle
+//		val connection = handle.connection
+//		val x = loc.x
+//		val y = loc.y
+//		val z = loc.z
+//
+//		if (handle.containerMenu !== handle.inventoryMenu) {
+//			handle.closeContainer()
+//		}
+//
+//		var teleportAwait: Int
+//		justTeleportedField.set(connection, true)
+//		teleportPosField.set(connection, Vec3(x, y, z))
+//		lastPosXField.set(connection, x)
+//		lastPosYField.set(connection, y)
+//		lastPosZField.set(connection, z)
+//		teleportAwait = teleportAwaitField.getInt(connection).plus(1)
+//
+//		if (teleportAwait == 2147483647) {
+//			teleportAwait = 0
+//		}
+//
+//		teleportAwaitField.set(connection, teleportAwait)
+//		AField.set(connection, eField.get(connection))
+//
+//		val px: Double
+//		val py: Double
+//		val pz: Double
+//
+//		if (offsetPos == null) {
+//			px = x
+//			py = y
+//			pz = z
+//		} else {
+//			px = offsetPos.x
+//			py = offsetPos.y
+//			pz = offsetPos.z
+//		}
+//
+//		handle.setPos(x, y, z)
+//		handle.setRot(handle.yRot + theta, handle.xRot)
+//
+////		handle.level.updateChunkPos(handle) // Removed with 1.17.1
+//
+//		val flags = if (offsetPos != null) OFFSET_ALL else OFFSET_DIRECTION
+//		val packet = ClientboundPlayerPositionPacket(px, py, pz, theta, 0f, flags, 0, true)
+////		old:	ClientboundPlayerPositionPacket(px, py, pz, theta, 0f, flags, teleportAwait)
+//		connection.send(packet)
+//	}
 
 	fun teleport(player: Player, loc: Location) {
 		move(player, loc, 0.0f, null)
