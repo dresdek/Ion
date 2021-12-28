79557caec4757f033c74d9df33acc24932a0666d
Peter-Crawley <peterzcrawley@gmail.com>
2021-12-28 16:02:19 +0000

    Paperweight 1.2.0 > 1.3.4-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
 	java
 	id("org.jetbrains.kotlin.jvm") version "1.6.10"
-	id("io.papermc.paperweight.userdev") version "1.2.0"
+	id("io.papermc.paperweight.userdev") version "1.3.4-SNAPSHOT"
 	id("com.github.johnrengelman.shadow") version "7.1.1"
 }
 
