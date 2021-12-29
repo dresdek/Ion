fcaab5e33615d43af3a5de1db11d88eb933a0ce9
Peter-Crawley <peterzcrawley@gmail.com>
2021-12-29 15:15:46 +0000

    Paper Dev Bundle provides paper-api

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,6 @@ repositories {
 }
 
 dependencies {
-	compileOnly("io.papermc.paper:paper-api:1.18.1-R0.1-SNAPSHOT")
 	paperDevBundle("1.18.1-R0.1-SNAPSHOT")
 
 	compileOnly("com.sk89q.worldedit:worldedit-bukkit:7.2.8") // https://github.com/EngineHub/WorldEdit
