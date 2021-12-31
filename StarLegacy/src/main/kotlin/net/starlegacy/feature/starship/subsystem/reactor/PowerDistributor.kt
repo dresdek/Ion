1e7c26b5390b4e7d464cd098a76b806c6ecdabc3
Peter-Crawley <peterzcrawley@gmail.com>
2021-12-31 21:51:41 +0000

    does not need to add up to 1 anymore

--- StarLegacy/src/main/kotlin/net/starlegacy/feature/starship/subsystem/reactor/PowerDistributor.kt
+++ StarLegacy/src/main/kotlin/net/starlegacy/feature/starship/subsystem/reactor/PowerDistributor.kt
@@ -9,10 +9,6 @@ class PowerDistributor {
 		private set
 
 	fun setDivision(shield: Double, weapon: Double, thruster: Double) {
-		check(shield + weapon + thruster == 1.0) {
-			"shield ($shield) + weapon ($weapon) + thruster ($thruster) must add up to 1"
-		}
-
 		this.shieldPortion = shield
 		this.weaponPortion = weapon
 		this.thrusterPortion = thruster
