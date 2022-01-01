b1535127b38ca66e15c6be553148a4ef15347b7f
Peter-Crawley <peterzcrawley@gmail.com>
2022-01-01 16:49:59 +0000

    Don't use a snapshot version

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -30,7 +30,7 @@ dependencies {
 	implementation("com.googlecode.cqengine:cqengine:3.6.0") // https://github.com/npgall/cqengine
 	implementation("com.github.jkcclemens:khttp:0.1.0") // https://github.com/jkcclemens/khttp
 	implementation("net.wesjd:anvilgui:1.5.3-SNAPSHOT")
-	implementation("co.aikar:acf-paper:0.5.1-SNAPSHOT") // https://github.com/aikar/commands
+	implementation("co.aikar:acf-paper:0.5.0") // https://github.com/aikar/commands
 	implementation("org.litote.kmongo:kmongo:4.4.0") // https://github.com/Litote/kmongo
 	implementation("redis.clients:jedis:3.7.1") // https://github.com/xetorthio/jedis
 }
