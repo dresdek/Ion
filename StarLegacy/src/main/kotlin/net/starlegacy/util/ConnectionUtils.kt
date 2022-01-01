ccb5a0b789950db5f14d6ae2015cc4eb031b9185
Peter-Crawley <peterzcrawley@gmail.com>
2022-01-01 01:00:01 +0000

    Lets bring back the reflection rubbish and hope that fixes it

--- StarLegacy/src/main/kotlin/net/starlegacy/util/ConnectionUtils.kt
+++ StarLegacy/src/main/kotlin/net/starlegacy/util/ConnectionUtils.kt
@@ -1,11 +1,14 @@
 package net.starlegacy.util
 
+import java.lang.reflect.Field
 import net.minecraft.network.protocol.game.ClientboundPlayerPositionPacket
 import net.minecraft.network.protocol.game.ClientboundPlayerPositionPacket.RelativeArgument.X
 import net.minecraft.network.protocol.game.ClientboundPlayerPositionPacket.RelativeArgument.X_ROT
 import net.minecraft.network.protocol.game.ClientboundPlayerPositionPacket.RelativeArgument.Y
 import net.minecraft.network.protocol.game.ClientboundPlayerPositionPacket.RelativeArgument.Y_ROT
 import net.minecraft.network.protocol.game.ClientboundPlayerPositionPacket.RelativeArgument.Z
+import net.minecraft.server.network.ServerGamePacketListenerImpl
+import net.minecraft.world.phys.Vec3
 import org.bukkit.Location
 import org.bukkit.craftbukkit.v1_18_R1.entity.CraftPlayer
 import org.bukkit.entity.Player
@@ -15,23 +18,51 @@ object ConnectionUtils {
 	private val OFFSET_DIRECTION = setOf(X_ROT, Y_ROT)
 	private val OFFSET_ALL = setOf(X_ROT, Y_ROT, X, Y, Z)
 
+	private var justTeleportedField = getField("justTeleported") // I do not know the obfuscated name of this field, lets hope this just works/
+	private var teleportPosField = getField("y") // awaitingPositionFromClient / teleportPos
+	private var lastPosXField = getField("o") // lastPosX / lastGoodX
+	private var lastPosYField = getField("u") // lastPosY / lastGoodY
+	private var lastPosZField = getField("q") // lastPosZ / lastGoodZ
+	private var teleportAwaitField = getField("z") // awaitingTeleport / teleportAwait
+
+	@Throws(NoSuchFieldException::class)
+	private fun getField(name: String): Field {
+		val field = ServerGamePacketListenerImpl::class.java.getDeclaredField(name)
+		field.isAccessible = true
+		return field
+	}
+
 	fun move(player: Player, targetLocation: Location, yawOffset: Float = 0.0f, offsetPos: Vector? = null) {
 		val serverPlayer = (player as CraftPlayer).handle
 		val connection = serverPlayer.connection
 
+		if (serverPlayer.containerMenu !== serverPlayer.inventoryMenu) serverPlayer.closeContainer()
+
+		justTeleportedField.set(connection, true)
+		teleportPosField.set(connection, Vec3(targetLocation.x, targetLocation.y, targetLocation.z))
+		lastPosXField.set(connection, serverPlayer.x)
+		lastPosYField.set(connection, serverPlayer.y)
+		lastPosZField.set(connection, serverPlayer.z)
+
+		var teleportAwait = teleportAwaitField.getInt(connection) + 1
+		if (teleportAwait == 2147483647) teleportAwait = 0
+		teleportAwaitField.set(connection, teleportAwait)
+
 		serverPlayer.setPos(targetLocation.x, targetLocation.y, targetLocation.z)
 		serverPlayer.setRot(serverPlayer.yRot + yawOffset, serverPlayer.xRot)
 
-		connection.send(ClientboundPlayerPositionPacket(
-			offsetPos?.x ?: targetLocation.x,
-			offsetPos?.y ?: targetLocation.y,
-			offsetPos?.z ?: targetLocation.z,
-			yawOffset,
-			0f,
-			if (offsetPos == null) OFFSET_DIRECTION else OFFSET_ALL,
-			0,
-			true
-		))
+		connection.send(
+			ClientboundPlayerPositionPacket(
+				offsetPos?.x ?: targetLocation.x,
+				offsetPos?.y ?: targetLocation.y,
+				offsetPos?.z ?: targetLocation.z,
+				yawOffset,
+				0f,
+				if (offsetPos == null) OFFSET_DIRECTION else OFFSET_ALL,
+				0,
+				true
+			)
+		)
 	}
 
 	fun teleport(player: Player, loc: Location) {
