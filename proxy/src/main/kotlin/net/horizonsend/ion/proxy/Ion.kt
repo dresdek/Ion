856a2afc71a312d436512b8719aae09718a218cd
Peter-Crawley <peterzcrawley@gmail.com>
2022-01-02 12:34:24 +0000

    Proxy: id = "ion" > "Ion" name = "Ion" > "Ion (Proxy)"

--- proxy/src/main/kotlin/net/horizonsend/ion/proxy/Ion.kt
+++ proxy/src/main/kotlin/net/horizonsend/ion/proxy/Ion.kt
@@ -22,7 +22,7 @@ import net.horizonsend.ion.proxy.commands.WarnCommand
 import net.horizonsend.ion.proxy.data.BanData
 import org.slf4j.Logger
 
-@Plugin(id = "ion", name = "Ion", version = "2.0.0", description = "Ion Proxy Plugin", authors = ["PeterCrawley"], url = "https://horizonsend.net")
+@Plugin(id = "Ion", name = "Ion (Proxy)", version = "1.0.0", description = "Ion (Proxy)", authors = ["PeterCrawley"], url = "https://horizonsend.net")
 class Ion @Inject constructor(val server: ProxyServer, logger: Logger, @DataDirectory val dataDirectory: Path) {
 	companion object {
 		lateinit var plugin: Ion
