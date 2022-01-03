50e97504466d4f9ce31fcdd78d99ca9d1d91c3b2
Peter-Crawley <peterzcrawley@gmail.com>
2022-01-03 18:46:58 +0000

    Return a collection, instead of specifically a set

--- proxy/src/main/kotlin/net/horizonsend/ion/proxy/Util.kt
+++ proxy/src/main/kotlin/net/horizonsend/ion/proxy/Util.kt
@@ -7,11 +7,11 @@ import kotlinx.serialization.json.Json
 import net.horizonsend.ion.proxy.Ion.Companion.ionInstance
 import net.horizonsend.ion.proxy.data.UUIDData
 
-fun targetsFromIonSelector(selector: String): Set<Player> =
-	if (selector == "*") ionInstance.server.allPlayers.toSet()
+fun targetsFromIonSelector(selector: String): Collection<Player> =
+	if (selector == "*") ionInstance.server.allPlayers
 	else if (selector.startsWith("@")) {
 		val targetServer = ionInstance.server.getServer(selector.substring(1)).orElse(null)
-		targetServer?.playersConnected?.toSet() ?: emptySet()
+		targetServer?.playersConnected ?: emptySet()
 	} else {
 		val targetPlayer = ionInstance.server.getPlayer(selector).orElse(null)
 		if (targetPlayer != null) setOf(targetPlayer) else emptySet()
