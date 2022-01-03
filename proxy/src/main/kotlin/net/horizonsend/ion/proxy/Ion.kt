c098ff1d471cd0615540dc870ab09673ca7176ce
Peter-Crawley <peterzcrawley@gmail.com>
2022-01-03 14:14:32 +0000

    Do everything in onStart instead of init

--- proxy/src/main/kotlin/net/horizonsend/ion/proxy/Ion.kt
+++ proxy/src/main/kotlin/net/horizonsend/ion/proxy/Ion.kt
@@ -5,22 +5,24 @@ import com.google.inject.Inject
 import com.velocitypowered.api.event.Subscribe
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent
 import com.velocitypowered.api.plugin.Plugin
+import com.velocitypowered.api.plugin.annotation.DataDirectory
 import com.velocitypowered.api.proxy.ProxyServer
+import java.nio.file.Path
 import net.horizonsend.ion.proxy.commands.MoveCommand
 import net.horizonsend.ion.proxy.commands.SwitchCommand
 import org.slf4j.Logger
 
 @Plugin(id = "ion", name = "Ion (Proxy)", version = "1.0.0", description = "Ion (Proxy)", authors = ["PeterCrawley"], url = "https://horizonsend.net")
-class Ion @Inject constructor(val server: ProxyServer, logger: Logger) {
+class Ion @Inject constructor(val server: ProxyServer, logger: Logger, @DataDirectory val dataDirectory: Path) {
 	companion object {
 		lateinit var ionInstance: Ion
 			private set
 	}
 
-	init { ionInstance = this }
-
 	@Subscribe
 	fun onStart(event: ProxyInitializeEvent) {
+		ionInstance = this
+
 		VelocityCommandManager(server, this).apply {
 			setOf(MoveCommand, SwitchCommand).forEach { registerCommand(it) }
 
