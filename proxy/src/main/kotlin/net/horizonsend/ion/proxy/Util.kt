f435a5914f9cf30e0320d2eebb0fc156c89ac72a
Peter-Crawley <peterzcrawley@gmail.com>
2022-01-03 15:30:23 +0000

    bring back an improved getUUIDFromName

--- proxy/src/main/kotlin/net/horizonsend/ion/proxy/Util.kt
+++ proxy/src/main/kotlin/net/horizonsend/ion/proxy/Util.kt
@@ -1,7 +1,12 @@
 package net.horizonsend.ion.proxy
 
 import com.velocitypowered.api.proxy.Player
+import java.net.URL
+import java.util.UUID
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 import net.horizonsend.ion.proxy.Ion.Companion.ionInstance
+import net.horizonsend.ion.proxy.data.UUIDData
 
 fun targetsFromIonSelector(selector: String): Set<Player> =
 	if (selector == "*") ionInstance.server.allPlayers.toSet()
@@ -11,4 +16,10 @@ fun targetsFromIonSelector(selector: String): Set<Player> =
 	} else {
 		val targetPlayer = ionInstance.server.getPlayer(selector).orElse(null)
 		if (targetPlayer != null) setOf(targetPlayer) else emptySet()
+	}
+
+// TODO: Cache these requests to avoid spamming the API
+fun getUUIDFromName(name: String): UUID =
+	URL("https://api.mojang.com/users/profiles/minecraft/$name").openStream().use {
+		UUID.fromString(Json.decodeFromString<UUIDData>(it.readAllBytes().toString()).id)
 	}
