a84c3c3d779f8890de5412d5b479d7e2dc15f899
Peter-Crawley <peterzcrawley@gmail.com>
2022-01-03 16:03:19 +0000

    Good chunk of the force link command

--- proxy/src/main/kotlin/net/horizonsend/ion/proxy/commands/Linking.kt
+++ proxy/src/main/kotlin/net/horizonsend/ion/proxy/commands/Linking.kt
@@ -3,6 +3,7 @@ package net.horizonsend.ion.proxy.commands
 import co.aikar.commands.BaseCommand
 import co.aikar.commands.annotation.CommandAlias
 import co.aikar.commands.annotation.CommandCompletion
+import co.aikar.commands.annotation.CommandPermission
 import co.aikar.commands.annotation.Description
 import com.velocitypowered.api.proxy.Player
 import kotlin.random.Random
@@ -23,6 +24,32 @@ object Linking: BaseCommand() {
 	fun new(source: Player, target: String) {
 		val sourceUUID = source.uniqueId.toString()
 
+		val nameAndUUIDPair = nameAndUUIDFromString(target)
+
+		// 34 = A
+		// 59 = Z
+		val code = (0..3).map { Char(Random.nextInt(34, 59)) }.joinToString("")
+
+		linkCodes.add(LinkCode(sourceUUID, nameAndUUIDPair.second, code, System.currentTimeMillis() + 300000)) // 1000 * 60 * 5 = 300000 ( 5 minutes )
+
+		source.sendMessage(text("Run /link $code on $target to link this account (${source.username}) to ${nameAndUUIDPair.first}. This link code will expire in 5 minutes."))
+	}
+
+	@CommandAlias("force")
+	@CommandCompletion("@players")
+	@Description("Forcefully links a player to a player.")
+	@CommandPermission("ion.linkadmin")
+	@Suppress("unused")
+	fun force(source: Player, target1: String, target2: String) {
+		val target1NameAndUUIDPair = nameAndUUIDFromString(target1)
+		val target2NameAndUUIDPair = nameAndUUIDFromString(target2)
+
+		TODO("Link accounts")
+
+		source.sendMessage(text("${target1NameAndUUIDPair.first} has been forcibly linked to ${target2NameAndUUIDPair.first}."))
+	}
+
+	private fun nameAndUUIDFromString(target: String): Pair<String, String> {
 		val targetPlayer = ionInstance.server.getPlayer(target).orElse(null)
 		val targetName: String
 		val targetUUID: String
@@ -37,12 +64,6 @@ object Linking: BaseCommand() {
 			targetUUID = targetPlayer.uniqueId.toString()
 		}
 
-		// 34 = A
-		// 59 = Z
-		val code = (0..3).map { Char(Random.nextInt(34, 59)) }.joinToString("")
-
-		linkCodes.add(LinkCode(sourceUUID, targetUUID, code, System.currentTimeMillis() + 300000)) // 1000 * 60 * 5 = 300000 ( 5 minutes )
-
-		source.sendMessage(text("Run /link $code on $target to link this account (${source.username}) to $targetName. This link code will expire in 5 minutes."))
+		return targetName to targetUUID
 	}
 }
