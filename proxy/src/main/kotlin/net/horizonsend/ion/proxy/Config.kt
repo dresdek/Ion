ce8c02672be061fc401cc2c2262e1583c3ca0233
Peter-Crawley <peterzcrawley@gmail.com>
2022-01-03 14:18:01 +0000

    Serializable

--- proxy/src/main/kotlin/net/horizonsend/ion/proxy/Config.kt
+++ proxy/src/main/kotlin/net/horizonsend/ion/proxy/Config.kt
@@ -1,5 +1,8 @@
 package net.horizonsend.ion.proxy
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Config(
 	val host: String = "localhost",
 	val username: String = "test",
