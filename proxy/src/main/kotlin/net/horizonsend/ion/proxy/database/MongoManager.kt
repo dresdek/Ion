8251f67e06b98f8b051ff5078434bf0f64152be0
Peter-Crawley <peterzcrawley@gmail.com>
2022-01-03 14:36:22 +0000

    Account Data Collection

--- proxy/src/main/kotlin/net/horizonsend/ion/proxy/database/MongoManager.kt
+++ proxy/src/main/kotlin/net/horizonsend/ion/proxy/database/MongoManager.kt
@@ -3,11 +3,15 @@ package net.horizonsend.ion.proxy.database
 import com.velocitypowered.api.event.Subscribe
 import com.velocitypowered.api.event.proxy.ProxyShutdownEvent
 import net.horizonsend.ion.proxy.Ion.Companion.ionConfig
+import net.horizonsend.ion.proxy.database.data.AccountData
 import org.litote.kmongo.KMongo.createClient
+import org.litote.kmongo.getCollection
 
 object MongoManager {
 	private val client = createClient("mongodb://${ionConfig.username}:${ionConfig.password}@${ionConfig.host}:${ionConfig.port}/${ionConfig.database}")
 	private val database = client.getDatabase(ionConfig.database)
+
+	val accountDataCollection = database.getCollection<AccountData>()
 
 	init {
 		// https://github.com/Litote/kmongo/issues/98
