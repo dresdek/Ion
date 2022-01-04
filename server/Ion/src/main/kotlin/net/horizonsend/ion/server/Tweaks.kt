bc39c1c8ce9f3e4fecf867b5c331e7c7f0c0c323
Peter-Crawley <peterzcrawley@gmail.com>
2022-01-04 17:52:13 +0000

    Do not save chunks if the world is equal to "SpaceArena"

--- server/Ion/src/main/kotlin/net/horizonsend/ion/server/Tweaks.kt
+++ server/Ion/src/main/kotlin/net/horizonsend/ion/server/Tweaks.kt
@@ -4,8 +4,14 @@ import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.CreatureSpawnEvent
 import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason.NETHER_PORTAL
+import org.bukkit.event.world.ChunkUnloadEvent
 
 object Tweaks: Listener {
+	@EventHandler
+	fun onSpaceArenaChunkUnload(event: ChunkUnloadEvent) {
+		if (event.world.name == "SpaceArena") event.isSaveChunk = false
+	}
+
 	@EventHandler
 	fun onCreatureSpawn(event: CreatureSpawnEvent) {
 		if (event.spawnReason == NETHER_PORTAL) event.isCancelled = true
