5d07be7195e93ebddae634283bf745321f0a4cbb
Peter-Crawley <peterzcrawley@gmail.com>
2022-01-05 19:28:28 +0000

    Remove unused

--- proxy/src/main/kotlin/net/horizonsend/ion/proxy/Util.kt
+++ proxy/src/main/kotlin/net/horizonsend/ion/proxy/Util.kt
@@ -1,11 +1,7 @@
 package net.horizonsend.ion.proxy
 
 import com.velocitypowered.api.proxy.Player
-import java.net.URL
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.json.Json
 import net.horizonsend.ion.proxy.Ion.Companion.ionInstance
-import net.horizonsend.ion.proxy.data.UUIDData
 
 fun targetsFromIonSelector(selector: String): Collection<Player> =
 	if (selector == "*") ionInstance.server.allPlayers // If * return all players
@@ -14,10 +10,4 @@ fun targetsFromIonSelector(selector: String): Collection<Player> =
 	} else { // Return the player (or none if the player doesn't exist)
 		val targetPlayer = ionInstance.server.getPlayer(selector).orElse(null)
 		if (targetPlayer != null) setOf(targetPlayer) else emptySet()
-	}
-
-// TODO: Cache these requests to avoid spamming the API
-fun getProfileFromName(name: String): UUIDData =
-	URL("https://api.mojang.com/users/profiles/minecraft/$name").openStream().use {
-		Json.decodeFromString(it.readAllBytes().toString())
 	}
