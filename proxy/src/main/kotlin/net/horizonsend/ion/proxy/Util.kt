fe995e6c5105d347f42e6ab106c6a4b566c527f9
Peter-Crawley <peterzcrawley@gmail.com>
2022-01-06 16:04:16 +0000

    Function to get a username using a players UUID

--- proxy/src/main/kotlin/net/horizonsend/ion/proxy/Util.kt
+++ proxy/src/main/kotlin/net/horizonsend/ion/proxy/Util.kt
@@ -1,7 +1,12 @@
 package net.horizonsend.ion.proxy
 
 import com.velocitypowered.api.proxy.Player
+import java.net.URL
+import java.util.UUID
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 import net.horizonsend.ion.proxy.Ion.Companion.ionInstance
+import net.horizonsend.ion.proxy.data.UsernameData
 
 fun targetsFromIonSelector(selector: String): Collection<Player> =
 	if (selector == "*") ionInstance.server.allPlayers // If * return all players
@@ -10,4 +15,9 @@ fun targetsFromIonSelector(selector: String): Collection<Player> =
 	} else { // Return the player (or none if the player doesn't exist)
 		val targetPlayer = ionInstance.server.getPlayer(selector).orElse(null)
 		if (targetPlayer != null) setOf(targetPlayer) else emptySet()
+	}
+
+fun nameFromUUID(uuid: UUID): String =
+	URL("https://api.mojang.com/user/profiles/$uuid/names").openStream().use {
+		Json.decodeFromString<List<UsernameData>>(it.readAllBytes().toString()).last().name
 	}
