b51183df734ab5a1492576cb3346cab929d3ef8e
Peter-Crawley <peterzcrawley@gmail.com>
2022-01-06 16:17:55 +0000

    Send message to the player in game (assuming they are still online)

--- proxy/src/main/kotlin/net/horizonsend/ion/proxy/JDAListener.kt
+++ proxy/src/main/kotlin/net/horizonsend/ion/proxy/JDAListener.kt
@@ -1,11 +1,14 @@
 package net.horizonsend.ion.proxy
 
+import java.util.UUID
 import net.dv8tion.jda.api.entities.ChannelType
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
+import net.horizonsend.ion.proxy.Ion.Companion.server
 import net.horizonsend.ion.proxy.commands.Link.validateLinkCode
 import net.horizonsend.ion.proxy.database.MongoManager.getAccountData
 import net.horizonsend.ion.proxy.database.MongoManager.saveAccountData
+import net.kyori.adventure.text.Component.text
 
 object JDAListener: ListenerAdapter() {
 	override fun onMessageReceived(event: MessageReceivedEvent) {
@@ -15,6 +18,12 @@ object JDAListener: ListenerAdapter() {
 
 				if (player != null) {
 					saveAccountData(getAccountData(player)?.apply { discordUserId = event.author.id } ?: throw IllegalStateException("Account data not found"))
+
+					event.message.reply("Your discord account has been linked to the minecraft account \"${nameFromUUID(player)}\". If this is a mistake please contact Horizon's End staff.")
+
+					server.getPlayer(UUID.fromString(player)).ifPresent{
+						it.sendMessage(text("Your minecraft account has been linked to the discord account \"${event.author.asTag}\". If this is a mistake please contact Horizon's End staff."))
+					}
 
 				} else event.message.reply("Link code is invalid, non-existent, or expired.")
 			} else event.message.reply("Message is too short or too long to be a verification code. Please resend the code.")
