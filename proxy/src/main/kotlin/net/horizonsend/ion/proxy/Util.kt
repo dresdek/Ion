219ba22a6bbc487d3077d81541d96fd14cf356b1
Peter-Crawley <peterzcrawley@gmail.com>
2022-01-06 16:08:33 +0000

    Correct return

--- proxy/src/main/kotlin/net/horizonsend/ion/proxy/Util.kt
+++ proxy/src/main/kotlin/net/horizonsend/ion/proxy/Util.kt
@@ -2,7 +2,6 @@ package net.horizonsend.ion.proxy
 
 import com.velocitypowered.api.proxy.Player
 import java.net.URL
-import java.util.UUID
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import net.horizonsend.ion.proxy.Ion.Companion.ionInstance
@@ -17,7 +16,7 @@ fun targetsFromIonSelector(selector: String): Collection<Player> =
 		if (targetPlayer != null) setOf(targetPlayer) else emptySet()
 	}
 
-fun nameFromUUID(uuid: UUID): String =
+fun nameFromUUID(uuid: String): String =
 	URL("https://api.mojang.com/user/profiles/$uuid/names").openStream().use {
 		Json.decodeFromString<List<UsernameData>>(it.readAllBytes().toString()).last().name
 	}
