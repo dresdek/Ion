30d68ad04340c776e868027d9c61fdb20cd88e85
Peter-Crawley <peterzcrawley@gmail.com>
2022-01-06 18:39:37 +0000

    Actually send the messages

--- proxy/src/main/kotlin/net/horizonsend/ion/proxy/JDAListener.kt
+++ proxy/src/main/kotlin/net/horizonsend/ion/proxy/JDAListener.kt
@@ -18,14 +18,14 @@ object JDAListener: ListenerAdapter() {
 				if (player != null) {
 					saveAccountData(getAccountData(player).apply { discordUserId = event.author.id })
 
-					event.message.reply("Your discord account has been linked to the minecraft account \"${nameFromUUID(player)}\". If this is a mistake please contact Horizon's End staff.")
+					event.message.reply("Your discord account has been linked to the minecraft account \"${nameFromUUID(player)}\". If this is a mistake please contact Horizon's End staff.").queue()
 
 					server.getPlayer(player).ifPresent{
 						it.sendMessage(text("Your minecraft account has been linked to the discord account \"${event.author.asTag}\". If this is a mistake please contact Horizon's End staff."))
 					}
 
-				} else event.message.reply("Link code is invalid, non-existent, or expired.")
-			} else event.message.reply("Message is too short or too long to be a verification code. Please resend the code.")
+				} else event.message.reply("Link code is invalid, non-existent, or expired.").queue()
+			} else event.message.reply("Message is too short or too long to be a verification code. Please resend the code.").queue()
 		}
 	}
 }
