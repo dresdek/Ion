f0271fda532270fe67872d1ad7b49d1512c5f3d6
Peter-Crawley <peterzcrawley@gmail.com>
2022-01-06 18:58:56 +0000

    Don't relink (plus cleanup)

--- proxy/src/main/kotlin/net/horizonsend/ion/proxy/JDAListener.kt
+++ proxy/src/main/kotlin/net/horizonsend/ion/proxy/JDAListener.kt
@@ -7,25 +7,38 @@ import net.horizonsend.ion.proxy.Ion.Companion.server
 import net.horizonsend.ion.proxy.commands.Link.validateLinkCode
 import net.horizonsend.ion.proxy.database.MongoManager.getAccountData
 import net.horizonsend.ion.proxy.database.MongoManager.saveAccountData
-import net.kyori.adventure.text.Component.text
+import net.kyori.adventure.text.Component
 
 object JDAListener: ListenerAdapter() {
 	override fun onMessageReceived(event: MessageReceivedEvent) {
-		if (event.isFromType(ChannelType.PRIVATE)) {
-			if (event.message.contentRaw.length == 4) {
-				val player = validateLinkCode(event.message.contentRaw)
+		if (!event.isFromType(ChannelType.PRIVATE)) return
 
-				if (player != null) {
-					saveAccountData(getAccountData(player).apply { discordUserId = event.author.id })
+		if (event.message.contentRaw.length != 4) {
+			event.message.reply("Message is too short or too long to be a verification code. Please resend the code.").queue()
+			return
+		}
 
-					event.message.reply("Your discord account has been linked to the minecraft account \"${nameFromUUID(player)}\". If this is a mistake please contact Horizon's End staff.").queue()
+		val player = validateLinkCode(event.message.contentRaw)
 
-					server.getPlayer(player).ifPresent{
-						it.sendMessage(text("Your minecraft account has been linked to the discord account \"${event.author.asTag}\". If this is a mistake please contact Horizon's End staff."))
-					}
+		if (player == null) {
+			event.message.reply("Link code is invalid, non-existent, or expired.").queue()
+			return
+		}
 
-				} else event.message.reply("Link code is invalid, non-existent, or expired.").queue()
-			} else event.message.reply("Message is too short or too long to be a verification code. Please resend the code.").queue()
+		getAccountData(player).apply {
+			if (discordUserId == null) {
+				event.message.reply("Your discord account has already been linked to a minecraft account. If this is a mistake please contact Horizon's End staff.").queue()
+				return
+			}
+
+			discordUserId = event.author.id
+			saveAccountData(this)
+
+			event.message.reply("Your discord account has been linked to the minecraft account \"${nameFromUUID(player)}\". If this is a mistake please contact Horizon's End staff.").queue()
+
+			server.getPlayer(player).ifPresent {
+				it.sendMessage(Component.text("Your minecraft account has been linked to the discord account \"${event.author.asTag}\". If this is a mistake please contact Horizon's End staff."))
+			}
 		}
 	}
 }
