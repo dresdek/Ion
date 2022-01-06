4fa4c74587b83699fa95e58da57be6ca6aaa8cd1
Peter-Crawley <peterzcrawley@gmail.com>
2022-01-06 16:11:09 +0000

    link accounts

--- proxy/src/main/kotlin/net/horizonsend/ion/proxy/JDAListener.kt
+++ proxy/src/main/kotlin/net/horizonsend/ion/proxy/JDAListener.kt
@@ -4,6 +4,8 @@ import net.dv8tion.jda.api.entities.ChannelType
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 import net.horizonsend.ion.proxy.commands.Link.validateLinkCode
+import net.horizonsend.ion.proxy.database.MongoManager.getAccountData
+import net.horizonsend.ion.proxy.database.MongoManager.saveAccountData
 
 object JDAListener: ListenerAdapter() {
 	override fun onMessageReceived(event: MessageReceivedEvent) {
@@ -12,7 +14,7 @@ object JDAListener: ListenerAdapter() {
 				val player = validateLinkCode(event.message.contentRaw)
 
 				if (player != null) {
-					// Link account
+					saveAccountData(getAccountData(player)?.apply { discordUserId = event.author.id } ?: throw IllegalStateException("Account data not found"))
 
 				} else event.message.reply("Link code is invalid, non-existent, or expired.")
 			} else event.message.reply("Message is too short or too long to be a verification code. Please resend the code.")
