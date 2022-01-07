c3009d12911a76d2fd75150774dab612fd634354
PeterCrawley1 <peterzcrawley@gmail.com>
2022-01-07 12:42:07 +0000

    Comments for PacketTweaks.kt

--- server/Ion/src/main/kotlin/net/horizonsend/ion/server/listeners/protocollib/PacketTweaks.kt
+++ server/Ion/src/main/kotlin/net/horizonsend/ion/server/listeners/protocollib/PacketTweaks.kt
@@ -1,5 +1,9 @@
 package net.horizonsend.ion.server.listeners.protocollib
 
+/**
+ * As part of code to enable ship movements to be seen beyond the normal render distance of the server, this event
+ * listener is used to lie to the client as to the server's render distance.
+ */
 object PacketTweaks: PacketAdapter(this, ListenerPriority.HIGHEST, PacketType.Play.Server.LOGIN) {
 	fun onPacketSending(event: PacketEvent) {
 		if (event.packetType != PacketType.Play.Server.LOGIN) return
