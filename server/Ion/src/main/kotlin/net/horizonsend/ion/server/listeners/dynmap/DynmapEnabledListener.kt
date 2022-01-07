232fa5fd8966b936aa69e0a58e94d9dca77f9605
PeterCrawley1 <peterzcrawley@gmail.com>
2022-01-07 12:43:23 +0000

    Comments for DynmapEnabledListener.kt

--- server/Ion/src/main/kotlin/net/horizonsend/ion/server/listeners/dynmap/DynmapEnabledListener.kt
+++ server/Ion/src/main/kotlin/net/horizonsend/ion/server/listeners/dynmap/DynmapEnabledListener.kt
@@ -1,5 +1,9 @@
 package net.horizonsend.ion.server.listeners.dynmap
 
+/**
+ * To make the dynmapAPI accessable to the plugin, we must wait for it to load, this event listener will be triggered
+ * when this happens.
+ */
 object DynmapEnabledListener: DynmapCommonAPIListener() {
 	override fun apiEnabled(api: DynmapCommonAPI) {
 		dynmapAPI = api as DynmapAPI
