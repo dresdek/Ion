2efa830354f9ec8211a7d94f17f00dbaac448f93
Peter-Crawley <peterzcrawley@gmail.com>
2022-01-08 00:12:42 +0000

    import

--- server/Ion/src/main/kotlin/net/horizonsend/ion/server/listeners/protocollib/LoginPacketListener.kt
+++ server/Ion/src/main/kotlin/net/horizonsend/ion/server/listeners/protocollib/LoginPacketListener.kt
@@ -1,6 +1,6 @@
 package net.horizonsend.ion.server.listeners.protocollib
 
-import com.comphenix.protocol.PacketType.Play.Server
+import com.comphenix.protocol.PacketType.Play.Server.LOGIN
 import com.comphenix.protocol.events.ListenerPriority.HIGHEST
 import com.comphenix.protocol.events.PacketAdapter
 import com.comphenix.protocol.events.PacketEvent
@@ -10,9 +10,9 @@ import net.horizonsend.ion.server.Ion.Companion.ionInstance
  * As part of code to enable ship movements to be seen beyond the normal render distance of the server, this event
  * listener is used to lie to the client as to the server's render distance.
  */
-object LoginPacketListener: PacketAdapter(ionInstance, HIGHEST, Server.LOGIN) {
+object LoginPacketListener: PacketAdapter(ionInstance, HIGHEST, LOGIN) {
 	override fun onPacketSending(event: PacketEvent) {
-		if (event.packetType != Server.LOGIN) return
+		if (event.packetType != LOGIN) return
 
 		event.packet.integers.write(2, 32) // Set render distance to 32
 	}
